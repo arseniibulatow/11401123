367d8ef96489c7b6a3d5d57627fd6e5a23d92249
arseniibulatow <arsenii.bulatow@gmail.com>
2025-12-17 20:10:13 +0300

    Update task3.cpp

--- Task3/task3.cpp
+++ Task3/task3.cpp
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <math.h> 
+double deg_to_rad(double degrees) {
+    return degrees * M_PI / 180.0;
+}
 
 int main() {
-double x1=0;
-double y1=0;
-double x2=1;
-double y2=1;
-double distance; 
-    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
-    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double a=4;
+    scanf("%lf", &a);
+    double b=3;
+    scanf("%lf", &b);
+    double C_degrees;
+    scanf("%lf", &C_degrees);
+    double C_rad = deg_to_rad(C_degrees);
+    double S = (a * b * sin(C_rad)) / 2.0;
+    double c_squared = a * a + b * b - 2 * a * b * cos(C_rad);
+    double c = sqrt(c_squared);
+    double L = a + b + c;
+    double h = b * sin(C_rad);
+    printf("%.4f\n", S);
+    printf("%.4f\n", L);
+    printf("%.4f\n", h);
 
-    printf("%.8f\n", distance);
-
-    return 0; 
+    return 0;
 }
+
